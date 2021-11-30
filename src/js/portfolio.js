0257dbedfbb07dc13ca1a4b1e302ec54f85587cf
Stephen Lei <stephen.lei@siriusxm.com>
2021-11-30 11:16:36 -0500

    adding portfolio funcitonlaity and assets

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -2,33 +2,58 @@ document.addEventListener('DOMContentLoaded', function () {
     fetch('./json/portfolio.json')
         .then(response => response.json())
         .then(data => {
-            // TODO: If url has hash then load those item details
+            // If url has hash then load those item details
+            const pageName = window.location.hash.substr(1);
+            if (pageName) {
+                const pageItem = data.work.find(item => item.name === pageName);
+                showDetails(pageItem);
+            }
 
             buildGrid(data);
         })
         .catch(error => console.log(error));
 });
 
+const imageBasePath = 'assets/image/portfolio/'
+
 // Builds portfolio grid
 function buildGrid(data) {
     const grid = document.getElementsByClassName('grid')?.[0];
     if (!!grid) {
-        data.work.forEach(el => {
-            const item = document.createElement('a');
-            item.href = `#${el.name}`;
-            item.onclick = (_) => { showDetails(el) };
+        data.work.forEach(item => {
+            const gridItemEl = document.createElement('a');
+            gridItemEl.href = `#${item.name}`;
+            gridItemEl.onclick = (_) => { showDetails(item) };
             const thumb = document.createElement('img');
-            thumb.src = `assets/image/portfolio/${el.thumbnail}`;
-            thumb.alt = `${el.alt}`;
-            item.appendChild(thumb);
-            grid.appendChild(item);
+            thumb.src = imageBasePath + item.thumb_src;
+            thumb.alt = `${item.alt_src}`;
+            gridItemEl.appendChild(thumb);
+            grid.appendChild(gridItemEl);
         });
     }
 }
 
 // Presents the details for a given portfolio item
 function showDetails(item) {
-    console.log(item);
-    const details = document.getElementsByClassName('portfolio-details')?.[0];
+    const detailsSection = document.getElementsByTagName('section')?.[0];
+    if (detailsSection) {
+        detailsSection.classList.remove('hide');
+    }
+    const detailsEl = document.getElementsByClassName('portfolio-details')?.[0];
+    const image = detailsEl.getElementsByClassName('image')?.[0];
+    const title = detailsEl.getElementsByClassName('title')?.[0];
+    const subtitle = detailsEl.getElementsByClassName('subtitle')?.[0];
+
+    image.src = imageBasePath + item.full_src;
+    title.innerHTML = item.title;
+    subtitle.innerHTML = item.subtitle;
+
+    if (item.orientation && item.orientation === 'landscape') {
+        image.classList.add('landscape');
+    } else {
+        image.classList.remove('landscape');
+    }
+
+    window.scrollTo({ top: 0, behavior: 'smooth' })
 }
 
